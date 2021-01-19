53bfa550e0f30463e983f1aa34af99eed29ebbce
yt-tian <1074965187@qq.com>
2021-01-20 00:39:35 +0800

    上传05配置脚手架

---
+++ 05配置脚手架/vuepro/src/components/child.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h1>child组件</h1>
+      <button @click="fn()">点击传自身数据给父</button>
+      <div>
+        引入父的数据 : {{cmsg}}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+   props:['cmsg'],
+   data(){
+       return{
+           val:'子的数据'
+       }
+   },
+   methods: {
+       fn(){
+           this.$emit("sendcmsg",this.val)
+       }
+   },
+}
+</script>
+
+<style>
+
+</style>
---
+++ 05配置脚手架/vuepro/src/components/parent.vue
@@ -0,0 +1,35 @@
+<template>
+     <div>
+         <h1>parent组件</h1>
+         <div>{{val}}</div>
+      <child1 :cmsg="pmsg" @sendcmsg="getval">
+      </child1>
+     </div>
+</template>
+
+<script>
+import child1 from './child.vue'
+// 引入child模板
+export default {
+    data(){
+        return {
+            pmsg:'父的数据',
+            val:''
+        }
+    },
+    // 创建组件
+    components:{
+        // 引入child组件
+        child1
+    },
+    methods: {
+        getval(inc){
+            this.val = inc
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
